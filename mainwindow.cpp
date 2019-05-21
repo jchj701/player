1095fc395e067fbbd3c23d483ce9fe4e7ccf654c
jchj701 <jchjkim@gmail.com>
2019-05-21 13:35:21 +0800

    commit test1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,7 +10,7 @@
 #include <QMessageBox>
 #include <QFileInfo>
 
-int MainWindow::model = 2;
+int MainWindow::model = 1;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
