9697130e8a9843413c6ffdb1b0ed84ae312cc4ed
jchj701 <jchjkim@gmail.com>
2019-05-21 09:02:27 +0800

    V1.0
    实现基本界面

---
+++ mainwindow.cpp
@@ -0,0 +1,278 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QFile>
+#include <QPainter>
+#include <QFileDialog>
+#include <QUrl>
+#include <QDebug>
+#include <QMediaMetaData>
+#include <QMessageBox>
+#include <QFileInfo>
+
+int MainWindow::model = 2;
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    timer1 = new QTimer(this);
+    timer2 = new QTimer(this);
+    add = false;
+    moved = 0;
+    player = new QMediaPlayer(this);
+    playerList = new QMediaPlaylist(this);
+
+    connect(player, SIGNAL(metaDataAvailableChanged(bool)), this, SLOT(showMessage(bool)));
+    connect(ui->processHorizontalSlider,SIGNAL(sliderMoved(int)), this,SLOT(seekChange(int)));
+    connect(player,SIGNAL(positionChanged(qint64)),this,SLOT(positionChange(qint64)));
+    qDebug() << "Music 构造函数";
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete player;
+    delete playerList;
+    delete ui;
+}
+
+
+void MainWindow::addMoremusic()
+{
+    QString musicFilePath, musicName;
+    QFileInfo info;
+//    list = new QListWidget(this);
+//    message = new QLabel(this);
+//    playerList = new QMediaPlaylist();
+//    player = new QMediaPlayer();
+    QStringList fileNameList = QFileDialog::getOpenFileNames (this, tr("打开文件"), "", tr("music(*.mp3)"));
+    if(!fileNameList.isEmpty ())
+    {
+        add = true;
+    }
+
+    //本地歌曲逐一加入playerList
+    for(int i = 0; i < fileNameList.size (); i++)
+    {
+        playerList->addMedia (QUrl::fromLocalFile (fileNameList.at(i)));
+
+        //获取歌曲信息
+        musicFilePath = fileNameList.at(i);
+        info = QFileInfo(musicFilePath);
+        musicName = info.fileName ();
+        //list->addItem (musicName);
+        //message->setText (tr("添加成功"));
+        qDebug() << "加入成功:" << fileNameList.at (i);
+
+        ui->listWidget->addItem (musicName);
+    }
+
+    playerList->setCurrentIndex (0);
+
+    musicPlayPattern();
+    player -> setPlaylist(playerList);
+
+}
+
+void MainWindow::setPlayModel()
+{
+    if(model == 0)
+    {
+        playerList->setPlaybackMode(QMediaPlaylist::Random);
+    }
+    else if(model == 1)
+    {
+        playerList->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+    }
+    else
+    {
+        playerList->setPlaybackMode(QMediaPlaylist::Sequential);
+    }
+}
+
+void MainWindow::playMusic()
+{
+    if(!add)
+    {
+        QMessageBox::information(this,"添加音乐","请先添加音乐","确定");
+    }
+    else
+    {
+        if(player -> state() == QMediaPlayer::PlayingState)//判断是否为播放模式
+        {
+            //设置播放模式为暂停
+            player -> pause();
+            timer1 ->stop();
+        }
+        else
+        {
+            //否则设置播放模式
+            player -> play();
+            connect(timer1, SIGNAL(timeout()), this, SLOT(posChange()));
+            timer1->start(1000);
+        }
+    }
+
+
+}
+
+void MainWindow::posChange()
+{
+
+    if(moved >= player -> duration())
+    {
+        qDebug() << "重新点击播放";
+        moved = 0;
+    }
+}
+
+void MainWindow::musicPlayPattern()
+{
+
+    //z=++z%3;
+    if(model == 0)
+    {
+        //顺序播放
+        playerList->setPlaybackMode(QMediaPlaylist::Sequential);
+        ui->pushButton_5->setText ("Sequential");
+
+    }
+    else if(model == 1)
+    {
+        //随机播放
+        playerList->setPlaybackMode(QMediaPlaylist::Random);
+        ui->pushButton_5->setText ("Random");
+    }
+
+    else
+    {
+        //单曲循环
+        model = 2;
+        playerList->setPlaybackMode(QMediaPlaylist::CurrentItemInLoop);
+        ui->pushButton_5->setText ("CurrentItemInLoop");
+    }
+    qDebug() << "now" << model;
+}
+
+void MainWindow::preMusic()
+{
+    //上一首，并更新播放时间为0
+    moved = 0;
+    playerList ->previous();
+
+}
+
+void MainWindow::nextMusic()
+{
+    //下一首，更新播放时间为0
+    moved = 0;
+    playerList->next();
+}
+
+void MainWindow::volumChange(int vol)
+{
+    //关联滑块的移动，将值设置播放的音量
+    player -> setVolume(vol);
+}
+
+void MainWindow::positionChange(qint64 position)
+{
+    qDebug() << "in positionChange";
+    ui->processHorizontalSlider->setMaximum(player->duration() / 1000);//设置滑块的长度范围为音乐长.00度
+    ui->processHorizontalSlider->setValue(position / 1000);//如果音乐进度改变，则改变滑块显示位置
+    moved = position;
+    qDebug() << "position = " << position;
+    qDebug() << "moved = " << moved;
+    QTime moveTime(0,(moved/60000) % 60,(moved / 1000) % 60);
+    ui->label_3->setText(moveTime.toString("mm:ss"));
+}
+
+void MainWindow::seekChange(int position)
+{
+    player->setPosition(position * 1000);//如果滑块改变位置，则改变音乐进度
+    moved = position * 1000;//让播放时间等于音乐进度（毫秒）
+    QTime moveTime(0,(moved/60000) % 60,(moved / 1000) % 60);//设置时间
+    ui->label_3->setText(moveTime.toString("mm:ss"));//显示时间
+}
+
+void MainWindow::showMessage(bool ok)
+{
+    if(ok)
+    {
+        qDebug() << "showMessage";
+        QString name= player->metaData(QMediaMetaData::Title).toString();
+        QString author= player->metaData(QMediaMetaData::Author).toString();
+        ui->textBrowser->setText("正在播放："+name + " -" + author);
+
+        QTime  displayTime(0,(player -> duration() / 60000) % 60,
+                           (player ->duration() / 1000) % 60);
+        ui->label_3->setText(displayTime.toString("mm:ss"));
+    }
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    addMoremusic ();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    playMusic();
+
+    //positionChange(100000);
+    //ui->textBrowser->setText (playerList->currentMedia ());
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    preMusic ();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    nextMusic ();
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    model--;
+    musicPlayPattern();
+}
+
+void MainWindow::on_verticalSlider_valueChanged(int value)
+{
+    if(!value)
+    {
+        ui->pushButton_6->setText ("Mute");
+    }
+    else {
+        ui->pushButton_6->setText ("Unmute");
+    }
+    volumChange(value);
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    if(ui->pushButton_6->text () == "Mute")
+    {
+        ui->pushButton_6->setText ("Unmute");
+        ui->verticalSlider->setValue (CurrentValue);
+        player->setMuted (false);
+    }
+    else {
+        CurrentValue = ui->verticalSlider->value ();
+        ui->pushButton_6->setText ("Mute");
+        ui->verticalSlider->setValue (0);
+        player->setMuted (true);
+    }
+}
+
+void MainWindow::on_processHorizontalSlider_sliderMoved(int position)
+{
+    qDebug() << "sliderMoved";
+    qDebug() << position;
+    seekChange(position);
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,95 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QMediaPlayer>
+#include <QPushButton>
+#include <QSlider>
+#include <QLabel>
+#include <QTime>
+#include <QPaintEvent>
+#include <QMediaPlaylist>
+#include <QTimer>
+#include <QListWidget>
+
+namespace Ui {
+    class MainWindow;
+    }
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    static int model;
+
+private slots:
+    void on_pushButton_4_clicked();
+    void addMoremusic();
+    void playMusic();
+    void preMusic();
+    void nextMusic();
+    void setPlayModel();
+//    void meteOpen();
+    void volumChange(int);
+    void positionChange(qint64 position);
+    void showMessage(bool);
+    void seekChange(int position);
+    void posChange();
+//    void clearMessage();
+    void musicPlayPattern();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_verticalSlider_valueChanged(int value);
+
+    void on_pushButton_6_clicked();
+
+    void on_processHorizontalSlider_sliderMoved(int position);
+
+private:
+    Ui::MainWindow *ui;
+//    void init_controls();
+//    void init_skin();
+
+//    QPushButton *BtnClose;
+//    QPushButton *BtnMin;
+//    QPushButton *BtnPlay;
+//    QPushButton *BtnPrev;
+//    QPushButton *BtnNext;
+//    QPushButton *muteButton;
+//    QPushButton *addMore;
+//    QPushButton *playPattern;
+//    QSlider     *volumeControl;
+//    QSlider     *seekSlider;
+//    QLabel      *showTime;
+//    QLabel      *showPro;
+//    QLabel      *showMge;
+//    QLabel      *title;
+    QLabel      *message;
+    QListWidget *list;
+    bool        add;
+    QTimer      *timer1;
+    QTimer      *timer2;
+    int         moved;
+//    QPoint      dragPosition;
+    QMediaPlayer   *player;
+    QMediaPlaylist * playerList;
+    int CurrentValue;
+
+protected:
+//    void paintEvent(QPaintEvent *event);
+//    void mousePressEvent(QMouseEvent *event);
+//    void mouseMoveEvent(QMouseEvent *event);
+};
+
+#endif // MAINWINDOW_H
